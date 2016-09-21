d73a7163112df40d93131c6de2b6f1b617521d30
Kartik Singhal <kartiksinghal@gmail.com>
2016-09-20 21:48:43 -0400

    Implement basic udp client

--- src/udp_client.rs
+++ src/udp_client.rs
@@ -5,6 +5,8 @@ extern crate log;
 extern crate env_logger;
 
 use clap::{App, Arg};
+use std::net::UdpSocket;
+use std::io::{self, Write};
 
 fn main() {
     env_logger::init().ok().expect("Failed to initialize logger");
@@ -16,4 +18,18 @@ fn main() {
             .index(1)
             .help("e.g. any port between 16384-16387"))
         .get_matches();
+
+    let port = matches.value_of("udpport").unwrap().parse::<u16>().unwrap();
+    info!("udpport: {}", port);
+
+    let socket = UdpSocket::bind(("127.0.0.1", port)).unwrap();
+
+    loop {
+        // TODO find out optimal buffer window to read
+        let mut buf = [0u8; 2048];
+        let (amt, _) = socket.recv_from(&mut buf).unwrap();
+        // TODO do I need a timeout here?
+
+        io::stdout().write(&buf[0..amt]).unwrap();
+    }
 }
