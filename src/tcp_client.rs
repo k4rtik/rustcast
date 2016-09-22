08c46d0bddc259b5ccbadc9266d1317d61a85085
Kartik Singhal <kartiksinghal@gmail.com>
2016-09-22 09:58:18 -0400

    Finish writing tcp client

--- src/tcp_client.rs
+++ src/tcp_client.rs
@@ -10,6 +10,7 @@ mod commands;
 use byteorder::{ByteOrder, BigEndian};
 use clap::{App, Arg};
 use commands::*;
+use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
@@ -31,11 +32,11 @@ fn main() {
         .get_matches();
 
     let servername = matches.value_of("servername").unwrap();
-    info!("server: {}", servername);
+    debug!("server: {}", servername);
     let serverport = matches.value_of("serverport").unwrap().parse::<u16>().unwrap();
-    info!("server port: {}", serverport);
+    debug!("server port: {}", serverport);
     let udpport = matches.value_of("udpport").unwrap().parse::<u16>().unwrap();
-    info!("udp port: {}", udpport);
+    debug!("udp port: {}", udpport);
 
     let mut stream = TcpStream::connect((servername, serverport)).unwrap();
 
@@ -46,7 +47,7 @@ fn main() {
     };
     hellobuf[0] = hello.command_type;
     BigEndian::write_u16(&mut hellobuf[1..], hello.udp_port);
-    info!("{:?}", hellobuf);
+    debug!("{:?}", hellobuf);
     stream.write_all(hellobuf.as_ref()).unwrap();
 
     let mut welcomebuf = [0u8; 3];
@@ -57,7 +58,93 @@ fn main() {
     };
     info!("{} {}", welcome.reply_type, welcome.num_stations);
 
-    loop {
+    println!("Type in a number to set the station we're listening to to that number.");
+    println!("Enter q or press CTRL+C to quit.");
+    println!("> The server has {} stations.", welcome.num_stations);
 
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(0) => {
+                info!("EndOfFile sent (Ctrl-D)");
+                break;
+            }
+            Ok(_) => {
+                match input.trim() {
+                    "q" => break,
+                    x => {
+                        let stationres = x.parse::<u16>();
+                        let station = match stationres {
+                            Ok(num) => num,
+                            Err(e) => {
+                                println!("Invalid input: number or 'q' expected");
+                                continue;
+                            }
+                        };
+
+                        let mut setstationbuf = [0u8; 3];
+                        let setstation = SetStation {
+                            command_type: 1,
+                            station_number: station,
+                        };
+                        setstationbuf[0] = setstation.command_type;
+                        BigEndian::write_u16(&mut setstationbuf[1..], setstation.station_number);
+                        debug!("{:?}", setstationbuf);
+                        stream.write_all(setstationbuf.as_ref()).unwrap();
+
+                        println!("Waiting for an announce…");
+
+                        let mut reply_type_buf = [0u8; 1];
+                        stream.read_exact(&mut reply_type_buf).unwrap();
+                        info!("{}", reply_type_buf[0]);
+                        match reply_type_buf[0] {
+                            0 => {
+                                error!("Server resent Welcome");
+                                break;
+                            }
+                            1 => {
+                                debug!("Announce");
+                                let mut song_name_size = [0u8; 1];
+                                stream.read_exact(&mut song_name_size).unwrap();
+                                info!("{}", song_name_size[0]);
+                                let song_name_size = song_name_size[0] as usize;
+                                let mut song_name = vec![0u8; song_name_size];
+                                stream.read_exact(&mut song_name).unwrap();
+
+                                println!("New song announced: {}",
+                                         String::from_utf8(song_name).unwrap());
+                            }
+                            2 => {
+                                // client sent an InvalidCommand
+                                let mut reply_string_size = [0u8; 1];
+                                stream.read_exact(&mut reply_string_size).unwrap();
+                                info!("{}", reply_string_size[0]);
+                                let mut reply_string = String::new();
+                                assert_eq!(reply_string_size[0] as usize,
+                                           stream.read_to_string(&mut reply_string).unwrap());
+                                info!("{}", reply_string);
+
+                                println!("INVALID_COMMAND_REPLY: {}", reply_string);
+                                println!("Server has closed the connection.");
+                                break;
+                            }
+                            _ => {
+                                error!("Server sent an unknown response");
+                                break;
+                            }
+
+                        };
+
+                        let mut commandbuf = [0u8; 2 + 255]; // size chosen to support maximum string len
+                    }
+                }
+            }
+            Err(_) => {
+                panic!("Unexpected error reading from stdin");
+            }
+        }
     }
 }
